d41b7e941b73dd959190ab791dfbb8ab1e51941b
bettercodevivek <viveksingh0520@gmail.com>
2025-06-08 15:00:06 +0530

    Implemented CartModel in models

---
+++ Models/CartModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const CartSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    items:[
+        {
+            product:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'Product',
+                required:true
+            },
+            quantity:{
+                type:Number,
+                min:1,
+                required:true
+            },
+            priceAtAddition:{
+                type:Number,
+                required:true
+            }
+        }
+    ],
+    totalPrice:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    updatedAt:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+const Cart = mongoose.model('Cart',CartSchema);
