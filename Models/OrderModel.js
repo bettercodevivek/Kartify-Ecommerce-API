aebb3555438580cc908f8f10ebea564cf6e7498e
bettercodevivek <viveksingh0520@gmail.com>
2025-06-08 13:53:59 +0530

    Added an important comment explaining the internal working of Schema and Model

--- Models/OrderModel.js
+++ Models/OrderModel.js
@@ -55,3 +55,27 @@ const OrderSchema = new mongoose.Schema({
 const Order = mongoose.model('Order',OrderSchema);
 
 module.exports = Order ;
+
+// Some Important Points to understand regarding Schema and model
+
+// when we create a new Schema 'new' keyword is used, but for creation of model 'new' is not used, why :
+
+// mongoose.Schema() is a class , toh jab hum new keyword use karke Schema create karte hai, we are creating an instance of this class
+// which basically means an object is being created, jo yeh batata hai ki document ka kya structure rahega when stored in DB
+// now this Schema is passed onto mongoose.model() , which is not a class but a factory method , which means ki yaha koi instance nahi
+// create ho raha rather model is being defined .
+// now when we use this model in other code like handler function to create a new order or new user like this :
+// const user = new User({}), yaha actually mein hum ek instance create kar rahe hai hamare model ka.
+
+// so internally what happens in mongoose : 
+
+// Tumhare schema ko register karta hai, model define karta hai
+
+// Ek class generate karta hai (model class)
+
+// Usse MongoDB collection se bind karta hai
+
+// Aur ek Model class return karta hai, ready to be used like:
+
+// Schema is an object, which is passed onto model and after model is defined ,
+//  a model class is generated which can be used to create its instances.
